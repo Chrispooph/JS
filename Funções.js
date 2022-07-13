74dccec3ceaba8c3cca550c6b761d70ae87e01be
Chrispooph <mrccrispa@gmail.com>
2022-07-13 10:43:34 -0300

    Console

--- Funções.js
+++ Funções.js
@@ -7,4 +7,4 @@ soma(3, 5);
 
 /* () - Declara parametros e valores.
 Console.log - Só quer ver um único valor e ver se a função é chamada.
-return - Chama a função e executa.
+return - Chama a função e executa. */
---
+++ Hello World.js
@@ -0,0 +1,14 @@
+console.log("Hello world!");
+
+function returnEvenValue(array) {
+    let evenNums = [];
+    for(let i = 0; i < array.lenght; i)
+    if(array[i] % 2 === 0){
+        evensNums.push(array[i]);
+    }
+    console.log(evenNums);
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8]
+returnEvenValue(array)
+// Chama uma função -> console.log("")
