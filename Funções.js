9ba9df0c93eae4309b7790ce5863d06e85d7ad5e
Chrispooph <mrccrispa@gmail.com>
2022-07-13 09:17:42 -0300

    Recursos básicos - Manipulando um arquivo.

---
+++ Funções.js
@@ -0,0 +1,10 @@
+function soma(a, b) {
+    console.log(a + b);
+    return a + b; // 8
+}
+
+soma(3, 5);
+
+/* () - Declara parametros e valores.
+Console.log - Só quer ver um único valor e ver se a função é chamada.
+return - Chama a função e executa.
