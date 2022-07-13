f58a2c26d62eb3fb058425255e394afb4f7221b9
Chrispooph <mrccrispa@gmail.com>
2022-07-13 09:12:20 -0300

    Recursos básicos - Manipulando um arquivo.

---
+++ Introdução - Manipulação.js
@@ -0,0 +1,14 @@
+// - Comentário de linha. 
+// var y = 0;
+
+/* Comentário de várias linhas.  
+    var x =1; */
+
+    var preço = 2;
+    var desconto = 0.2;
+    const PREÇO = 2;
+    var total = PREÇO - desconto
+    
+
+/* Constante - Não muda seu valor
+Váriavel - Altera seu valor. */
