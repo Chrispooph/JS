aac81475c33948474fe50bc8cf3153c92f723a6a
Crispa <mrccrispa@gmail.com>
2022-07-19 21:31:46 -0300

    Reprodução - Aulas práticas

---
+++ Funções em JavaScript/Atividade Prática2.js
@@ -0,0 +1,25 @@
+function calculeIdade(anos) {
+    return `Daqui a ${anos} anos, ${this.nome} terá ${
+        this.idade + anos
+    } anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: "Maria",
+    idade: 30
+}
+
+const pessoa2 = {
+    nome: "Carla",
+    idade: 26
+}
+
+const animal = {
+    nome: "Fiona",
+    idade: 5,
+    raca: "Pug",
+};
+
+console.log(calculeIdade.apply(pessoa1, [26]));
+console.log(calculeIdade.call(pessoa2, 30));
+console.log(calculeIdade.call(animal, 7));
